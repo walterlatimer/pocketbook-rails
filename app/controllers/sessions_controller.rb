5d445cfa89945e3e516cc27ebc559ec64129d8f0
Walter Latimer, Jr <walter@walterlatimer.com>
2015-03-08 15:07:43 -0400

    Add login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		@user = User.find_by(email: session_params[:email])
+		if @user && @user.authenticate(session_params[:password])
+			session[:user_id] = @user.id
+			redirect_to about_path
+		else
+			redirect_to login_path
+		end
+	end
+
+	private
+
+	def session_params
+		params.require(:session).permit(:email, :password)
+	end
+
+end
