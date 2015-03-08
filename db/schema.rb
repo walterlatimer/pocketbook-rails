c184acb3de9e6cad2cb3793df57e48f292eee810
Walter Latimer, Jr <walter@walterlatimer.com>
2015-03-08 13:04:21 -0400

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208204414) do
+ActiveRecord::Schema.define(version: 20150308165226) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
